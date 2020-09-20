5cdc1700228de3499c83c4f72fc4275cffa23e20
JeroenVerstraelen <jeroen.verstraelen@student.kuleuven.be>
2020-09-20 12:17:05 +0200

    New tables added

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ var app = require('express')(); // Create an instance of an Express app
 
 var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobile App with default settings
 
-mobileApp.tables.add('Person'); // Create a table for 'Person' with default settings
+mobileApp.tables.add('Person');
+mobileApp.tables.add('Movie');
+mobileApp.tables.add('PersonLikesMovie');
 
 app.use(mobileApp);
 app.listen(process.env.PORT || 3000);
